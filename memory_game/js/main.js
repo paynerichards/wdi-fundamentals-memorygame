8ce6f097dc8dff26f3d193da3e75ccc53442872b
Payne Richards <paynerichards@Paynes-MacBook-Pro.local>
2017-05-21 01:03:47 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+var cardOne = "queen"; 
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king"; //set card values
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree); //display values of two flipped cards
