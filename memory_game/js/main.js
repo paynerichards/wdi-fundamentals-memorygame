3578988a316068f61c641119ee10ac160cc32aba
Payne Richards <paynerichards@Paynes-MacBook-Pro.local>
2017-05-21 17:16:17 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
-var cardOne = "queen"; 
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king"; //set card values
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne)
+
+var cardTwo = cards[1]
+cardsInPlay.push(cardTwo)
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree); //display values of two flipped cards
+console.log("User flipped " + cardTwo); 
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+	} else {alert("Sorry, try again")
+}
